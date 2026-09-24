3edb7dd6394edd2243419ec49ccf8e85b2367555
Classiolabs <classiolabs@gmail.com>
2026-09-24 22:34:55 +0530

    mingw-w64: pin to v13.0.0 (newer headers break LLVM 20 libunwind)

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,5 +1,6 @@
 ExternalProject_Add(mingw-w64
     GIT_REPOSITORY https://github.com/mingw-w64/mingw-w64.git
+    GIT_TAG v13.0.0
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
