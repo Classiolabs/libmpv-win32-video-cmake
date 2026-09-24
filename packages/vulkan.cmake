f45ba1ffeaf5bdb373477d5b2e7e73943f7a0a03
Classiolabs <classiolabs@gmail.com>
2026-09-25 00:33:39 +0530

    vulkan: drop GIT_REMOTE_NAME so tag pin doesn't break force-update

--- packages/vulkan.cmake
+++ packages/vulkan.cmake
@@ -4,7 +4,6 @@ ExternalProject_Add(vulkan
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    GIT_REMOTE_NAME origin
     GIT_TAG v1.3.295
     PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-*.patch
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
